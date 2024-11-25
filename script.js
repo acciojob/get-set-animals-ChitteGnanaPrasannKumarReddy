eb635ef6e1b29fbe7815563945ca55757811a621
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-11-25 20:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,27 +3,41 @@ class Animal {
     this._species = species;
   }
 
+  
   get species() {
     return this._species;
   }
 
+  
   makeSound() {
-    console.log(`The ${this._species} makes a sound.`);
+    console.log(`The ${this._species} makes a sound`);
   }
 }
 
+
 class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  
   purr() {
     console.log("purr");
   }
 }
 
+
 class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  
   bark() {
     console.log("woof");
   }
 }
-
+ 
 
 // Do not change the code below this line
 window.Animal = Animal;
